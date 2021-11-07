ec92abb82f1c9d860ace0b6c9771bb856c5a2e83
Windy-Secker <75884241+Windy-Secker@users.noreply.github.com>
2021-11-07 12:05:12 +0800

    Add files via upload

---
+++ js/ini.js
@@ -0,0 +1,27 @@
+function ini1(){
+    document.getElementById('ci').href='stylesheets/s1.css';
+}
+
+function ini2(){
+    document.getElementById('ci').href='stylesheets/s2.css';
+}
+
+function ini3(){
+    document.getElementById('ci').href='stylesheets/s3.css';
+}
+
+function ini4(){
+    document.getElementById('ci').href='stylesheets/s4.css';
+}
+
+function ini5(){
+    document.getElementById('ci').href='stylesheets/s5.css';
+}
+
+function ini6(){
+    document.getElementById('ci').href='stylesheets/s6.css';
+}
+
+function ini7(){
+    document.getElementById('ci').href='stylesheets/s7.css';
+}
--- js/load.js
+++ js/load.js
@@ -3,12 +3,14 @@ function loadmain(){
   document.getElementById("search").style.display="none"
   document.getElementById("about").style.display="none"
   document.getElementById("su").style.display="none"
+  document.getElementById("ini").style.display="none"
 }
 
 function loadsearch(){
   document.getElementById("main").style.display="none"
   document.getElementById("search").style.display="block"
   document.getElementById("su").style.display="inline-block"
+  document.getElementById("ini").style.display="none"
   document.getElementById("about").style.display="none"
 }
 
@@ -17,6 +19,15 @@ function loadabout(){
   document.getElementById("search").style.display="none"
   document.getElementById("su").style.display="none"
   document.getElementById("about").style.display="inline"
+  document.getElementById("ini").style.display="none"
+}
+
+function loadini(){
+  document.getElementById("main").style.display="none"
+  document.getElementById("search").style.display="none"
+  document.getElementById("su").style.display="none"
+  document.getElementById("about").style.display="none"
+  document.getElementById("ini").style.display="block"
 }
 
 function loadlearn(){
